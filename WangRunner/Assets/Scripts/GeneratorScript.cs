9c41af6d2045d5b337b9cfb81581f0fe3f542b4e
eejhey <edee539@gmail.com>
2015-09-07 21:55:42 -0700

    Infinite side scroll with tap to jump
    It's glitchy though, needs to be brushed up.

--- WangRunner/Assets/Scripts/GeneratorScript.cs
+++ WangRunner/Assets/Scripts/GeneratorScript.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GeneratorScript : MonoBehaviour {
-	public GameObject[] levels;
+	public GameObject[] availableLevels;
+	public List<GameObject> currentLevels;
+
+	private float screenWidthInPoints;
 
 	// Use this for initialization
 	void Start () {
-	
+		float height = 2.0f * Camera.main.orthographicSize;
+		screenWidthInPoints = height * Camera.main.aspect;
 	}
 
 	void FixedUpdate(){
+		GenerateRoomIfRequired ();
+	}
 
+	void GenerateRoomIfRequired(){
+		List<GameObject> roomsToRemove = new List<GameObject>();
+		bool addRooms = true;
+		float playerX = transform.position.x;
+		float removeRoomX = playerX - screenWidthInPoints;
+		float addRoomX = playerX + screenWidthInPoints;
+		float farthestRoomEndX = 0;
+
+		foreach (var room in currentLevels) {
+			float roomWidth = 0.0f * room.transform.Find ("SmallGround").localScale.x;
+			float roomStartX = room.transform.position.x - (roomWidth * 0.5f);
+			Debug.Log( string.Format("roomStartX: {0} screenWidth: {1}", roomStartX, screenWidthInPoints) );
+			float roomEndX = roomStartX + roomWidth;
+
+			if( roomStartX > addRoomX )
+				addRooms = false;
+
+			if( roomEndX < removeRoomX )
+				roomsToRemove.Add (room);
+
+			farthestRoomEndX = Mathf.Max (farthestRoomEndX, roomEndX);
+		}
+
+		foreach (var room in roomsToRemove) {
+			currentLevels.Remove (room);
+			Destroy (room);
+		}
+
+		if (addRooms)
+			AddRoom (farthestRoomEndX);
+	}
+	
+	void AddRoom( float farthestRoomEndX ){
+		int randomRoomIndex = Random.Range (0, availableLevels.Length);
+		GameObject room = (GameObject)Instantiate (availableLevels [randomRoomIndex]);
+		float roomWidth = room.transform.Find ("SmallGround").localScale.x;
+		float roomCenter = farthestRoomEndX + (roomWidth * 0.5f);
+
+		room.transform.position = new Vector3 (roomCenter, 0, 0);
+		currentLevels.Add (room);
 	}
 }
--- WangRunner/Assets/Scripts/PlayerController.cs
+++ WangRunner/Assets/Scripts/PlayerController.cs
@@ -4,27 +4,34 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
-	bool isJumping = false;
-	Rigidbody2D rb;
+	public float movementSpeed = 3.0f;
+	public float jumpForce = 200.0f;
+	public Transform groundCheckTransform;
+	public LayerMask groundCheckLayerMask;
+	
+	private bool grounded = false;
+	private Rigidbody2D rb;
+	private float clickDelay;
 
-	public List<BoxCollider2D> platforms = new List<BoxCollider2D>();
+	//public List<BoxCollider2D> platforms = new List<BoxCollider2D>();
 	
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		//ground = GetComponent<BoxCollider2D> ();
 
-		GameObject[] groundStuff = GameObject.FindGameObjectsWithTag ("Ground");
+		/*GameObject[] groundStuff = GameObject.FindGameObjectsWithTag ("Ground");
 
 		for (int i = 0; i < groundStuff.Length; i++) {
 			platforms.Add(groundStuff[i].GetComponent<BoxCollider2D>());
-		}
+		}*/
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
 
-		for (int i = 0; i < platforms.Count; i++) {
+		/*for (int i = 0; i < platforms.Count; i++) {
 			if(rb.IsTouching(platforms[i]))
 			{
 				isJumping = false;
@@ -36,7 +43,28 @@ public class PlayerController : MonoBehaviour {
 			rb.AddForce (new Vector2 (0, 200));
 			transform.Translate(new Vector3(0, 1, 0));
 			isJumping = true;
+		}*/
+
+	}
+
+	void FixedUpdate(){
+		Vector2 newVelocity = rb.velocity;
+
+		newVelocity.x = movementSpeed;
+		rb.velocity = newVelocity;
+
+		clickDelay -= Time.fixedDeltaTime;
+		bool pressed = Input.GetButtonDown ("Fire1") && (clickDelay <= 0);
+
+		if(pressed && grounded){
+			clickDelay = 1.0f;
+			rb.AddForce (new Vector2 (0, jumpForce));
 		}
 
+		UpdateGroundedStatus ();
+	}
+
+	void UpdateGroundedStatus(){
+		grounded = Physics2D.OverlapCircle (groundCheckTransform.position, 0.1f, groundCheckLayerMask);
 	}
 }
