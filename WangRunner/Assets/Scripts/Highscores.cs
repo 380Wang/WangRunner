b0e6d1787928f395e9ac19bfed340f7350b431fd
Brian A. Canedo <bcanedo4@gmail.com>
2015-11-11 14:37:29 -0800

    Work on Highscores
    yeah

---
+++ WangRunner/Assets/Scripts/Highscores.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Highscores : MonoBehaviour {
+	public Text currentScore;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateScore()
+	{
+		currentScore.text = "Distance: " + ((int)transform.position.x + 3) + "m";
+	}
+}
