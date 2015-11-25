db9b02ff4833a755dc45a42d97399dc2c8679563
Ben Villalobos <villalobosb93@gmail.com>
2015-11-24 17:00:00 -0800

    ShopScreen Layout Done
    Scrolls Properly now, will adjust to any number of objects in the scroll
    area

--- WangRunner/Assets/Scripts/ShopInput.cs
+++ WangRunner/Assets/Scripts/ShopInput.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class ShopInput : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
