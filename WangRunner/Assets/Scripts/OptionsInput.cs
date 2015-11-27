466f5b1552fe635c1d3fe3e982171e1913e1e5ed
Ben Villalobos <villalobosb93@gmail.com>
2015-11-27 13:44:08 -0800

    Shop Screen Complete
    Saving items when bought is done
    Can't buy items if you don't have enough money
    If you press left crtl on the shop screen you get 10000 coins (debug
    purposes)
    Added a reset data button on the options screen

--- WangRunner/Assets/Scripts/OptionsInput.cs
+++ WangRunner/Assets/Scripts/OptionsInput.cs
@@ -5,19 +5,45 @@ using UnityEngine.UI;
 public class OptionsInput : MonoBehaviour {
 
     Slider musicSlider;
-	// Use this for initialization
+    GameObject messageBox;
 	void Start () {
+        messageBox = GameObject.Find("MessageBox");
+        messageBox.SetActive(false);
         musicSlider = GameObject.Find("Music Slider").GetComponent<Slider>();
         AudioListener.volume = PlayerPrefs.GetFloat("musicVolume", 1);
         Debug.Log(PlayerPrefs.GetFloat("musicVolume", 100));
     }
 	
-	// Update is called once per frame
+	
 	void Update () { 
 		musicSlider.value=AudioListener.volume;
-
-	
 	}
+
+    public void ShowMessageBox()
+    {
+        messageBox.SetActive(true);
+    }
+
+    
+    public void HideMessageBox()
+    {
+         messageBox.SetActive(false);
+    }
+
+    public void ResetData()
+    {
+        PlayerPrefs.SetInt("Glider", 0);
+        PlayerPrefs.SetInt("AirStabilizer", 0);
+        PlayerPrefs.SetInt("BoostJet", 0);
+        PlayerPrefs.SetInt("Uppercut", 0);
+        PlayerPrefs.SetInt("DiveKick", 0);
+        PlayerPrefs.SetInt("GrapplingHook", 0);
+        PlayerPrefs.SetInt("Jetpack", 0);
+
+        PlayerPrefs.SetInt("CurrentCoins", 0);
+
+        HideMessageBox();
+    }
 
 	public void PressedCreditsButton()
 	{
@@ -30,11 +56,27 @@ public class OptionsInput : MonoBehaviour {
 		Application.LoadLevel ("Main Menu");
 	}
 
+	public void DecVolume()
+	{
+			if (AudioListener.volume > 0) {
+			AudioListener.volume = (AudioListener.volume - 0.1F);
+			PlayerPrefs.SetFloat("musicVolume", AudioListener.volume + .01f);
+		}
+	}
+
+	public void IncVolume()
+	{
+		if (AudioListener.volume < 1) 
+		{
+			AudioListener.volume = (AudioListener.volume + 0.1F);
+			PlayerPrefs.SetFloat("musicVolume", AudioListener.volume + .01f);
+		}
+	}
+
 	public void SliderChange()
 	{
 		AudioListener.volume = musicSlider.value;
         PlayerPrefs.SetFloat("musicVolume", AudioListener.volume + .01f);
-        Debug.Log(PlayerPrefs.GetFloat("musicVolume", 100));
     }
 
 }
